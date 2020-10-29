654f8cede546693ceb1ba4b722456282bbb21f11
17254zeljkomiloradovic <zeljko.@elfak.rs>
2020-10-29 20:43:25 +0100

    Lokalno povezani

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,12 @@
-import web_search
+import flask
 
-proba=web_search.Webtender()
-proba.login()
+app = flask.Flask("__main__")
 
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return flask.render_template("index.html")
+
+@app.route('/settings')
+
+app.run(debug=True)
---
+++ backend/static/react/precache-manifest.3aa5ab4d2e9fde7af9223a51d8536c61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b90f15e02229324eb4304aa60db750",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "61bf1dac9ed3d21e41a6",
+    "url": "/static/react/static/css/main.55f1dfa4.chunk.css"
+  },
+  {
+    "revision": "a3a27b4ac335c28a29bc",
+    "url": "/static/react/static/js/2.fc0c0d3c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/react/static/js/2.fc0c0d3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61bf1dac9ed3d21e41a6",
+    "url": "/static/react/static/js/main.21502968.chunk.js"
+  },
+  {
+    "revision": "d963f178e7bb2d1e2c6c",
+    "url": "/static/react/static/js/runtime-main.28a1e2dd.js"
+  }
+]);
